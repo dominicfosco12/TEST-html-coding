9895928f42bf247ad1bf20edbc4cf4a58a903ed5
dominicfosco12 <160334913+dominicfosco12@users.noreply.github.com>
2025-07-25 17:36:20 -0400

    remove app

--- app.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(<HelloMOD />);
-}
-
-// You can either import or define HelloMOD inline:
-import HelloMOD from './components/HelloMOD.jsx';
