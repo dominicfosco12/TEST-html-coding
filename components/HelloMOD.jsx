2633a123c18f730064d815c4b6c2564788501f7c
dominicfosco12 <160334913+dominicfosco12@users.noreply.github.com>
2025-07-25 17:39:53 -0400

    one fille

--- components/HelloMOD.jsx
+++
@@ -1,8 +0,0 @@
-export default function HelloMOD() {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>⚡️ Welcome to MOD</h1>
-      <p>This is the beginning of your cloud-native OMS/PMS/EMS platform.</p>
-    </div>
-  );
-}
